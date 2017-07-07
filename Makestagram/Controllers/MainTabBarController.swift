962d8f5b54988d3bebbe0c481a23b32b26b6ee16
Mariano <mmontori@umich.edu>
2017-07-07 10:25:47 -0400

    taking photos

---
+++ Makestagram/Controllers/MainTabBarController.swift
@@ -0,0 +1,51 @@
+//
+//  MainTabBarController.swift
+//  Makestagram
+//
+//  Created by Mariano Montori on 7/7/17.
+//  Copyright © 2017 Mariano Montori. All rights reserved.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+
+    let photoHelper = MGPhotoHelper()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        photoHelper.completionHandler = { image in
+            print("handle image")
+        }
+        
+        delegate = self
+        tabBar.unselectedItemTintColor = .black
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+extension MainTabBarController: UITabBarControllerDelegate {
+    func tabBarController(_ tabBarController: UITabBarController, shouldSelect viewController: UIViewController) -> Bool {
+        if viewController.tabBarItem.tag == 1 {
+            photoHelper.presentActionSheet(from: self)
+            return false
+        }
+        return true
+    }
+}
